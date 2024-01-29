ddaca58707ba594b215a4759a0b42ae4ef3cd656
Gru <78332542+gru2007@users.noreply.github.com>
2024-01-29 22:11:13 +1000

    oh.. this is conflict with another discord plugin

--- plugin.rb
+++ plugin.rb
@@ -17,7 +17,7 @@ gem 'http-cookie','1.0.3'
 gem 'http-accept', '1.7.0', { require: false }
 gem 'rest-client', '2.1.0.rc1'
 
-gem 'discordrb-webhooks', '3.3.0', {require: false}
+gem 'discordrb-webhooks', '3.5.0', {require: false}
 gem 'discordrb', '3.4.0'
 
 #require 'discordrb'
